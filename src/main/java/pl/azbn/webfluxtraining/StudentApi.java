ae439b6ae7de8dc7a9116704bf2ecf8ccec169d6
ABogdan <Arkadiusz.Bogdan@external.t-mobile.pl>
2023-01-07 16:23:55 +0100

    Flux and Mono Reactive Stream

---
+++ src/main/java/pl/azbn/webfluxtraining/StudentApi.java
@@ -0,0 +1,37 @@
+package pl.azbn.webfluxtraining;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+public class StudentApi {
+
+    private Flux<Student> studentFlux;
+
+    public StudentApi() {
+        studentFlux = Flux.just(
+                new Student("Jan", "Nowak"),
+                new Student("Marysia", "Zdun"),
+                new Student("Karolina", "Wiśniewska")
+        );
+    }
+
+    @GetMapping(value = "/getstudents", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Student> getStudent() {
+        return studentFlux.delayElements(Duration.ofSeconds(2));
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Student> addStudent(@RequestBody Student student) {
+        studentFlux = studentFlux.mergeWith(Mono.just(student));
+        return studentFlux;
+    }
+
+}
